97c9c7c5953b10da4b5d8dee5666c08158b72a92
irammini <nhatlong23461@gmail.com>
2026-02-12 15:01:44 +0700

    Simpler Ripgrep installation

--- scripts/ripgrep.sh
+++ scripts/ripgrep.sh
@@ -1,9 +1,3 @@
-RIPGREP_VERSION=15.1.0
-FILE_NAME=ripgrep_$RIPGREP_VERSION-1_amd64.deb
-URL=https://github.com/BurntSushi/ripgrep/releases/download/$RIPGREP_VERSION/$FILE_NAME
-
-curl -O $URL
-sudo dpkg -i $FILE_NAME
-sudo rm -rf $FILE_NAME
+sudo apt-get install ripgrep -y
 
 echo "Installed Ripgrep."
