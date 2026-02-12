04100284db428bb83b45e85e691e62093020d920
irammini <nhatlong23461@gmail.com>
2026-02-13 04:30:03 +0700

    add duf

---
+++ scripts/duf.sh
@@ -0,0 +1,3 @@
+sudo apt install duf -y
+
+echo "Installed Duf."
--- scripts/procs.sh
+++ scripts/procs.sh
@@ -2,6 +2,7 @@ PROCS_VERSION=0.14.10
 FILE_NAME=procs-v${PROCS_VERSION}-x86_64-linux.zip
 URL=https://github.com/procs/procs/releases/download/v${PROCS_VERSION}/${FILE_NAME}
 
+sudo apt install unzip -y
 curl -O $URL
 sudo rm -rf /usr/local/bin/procs
 sudo unzip -d /usr/local/bin/ $FILE_NAME
