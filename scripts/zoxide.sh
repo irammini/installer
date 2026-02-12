040fb9e955a4ef2c611969f54b58adcca23b7051
irammini <nhatlong23461@gmail.com>
2026-02-12 14:40:35 +0700

    changes on zoxide

--- scripts/zoxide.sh
+++ scripts/zoxide.sh
@@ -3,6 +3,8 @@ curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh 
 cat << 'EOF' >> ~/.bashrc
 
 eval "$(zoxide init bash)"
+export PATH=$HOME/.local/bin:$PATH
 EOF
 
+sudo apt install fzf -y
 echo "Installed Zoxide. Please run 'source ~/.bashrc' to start using Zoxide."
