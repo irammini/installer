cc0eecbf6b09a5e84b3f2297f63aee4dc77852ad
irammini <nhatlong23461@gmail.com>
2026-02-12 15:08:10 +0700

    add procs

---
+++ scripts/procs.sh
@@ -0,0 +1,12 @@
+PROCS_VERSION=0.14.10
+FILE_NAME=procs-v${PROCS_VERSION}-86_64-linux.zip
+URL=https://github.com/procs/procs/releases/download/v${PROCS_VERSION}/${FILE_NAME}
+
+curl -O $URL
+sudo rm -rf /usr/local/bin/procs
+sudo unzip -d /usr/local/bin/ $FILE_NAME
+sudo rm -rf $FILE_NAME
+
+echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
+
+echo "Installed Procs version $PROCS_VERSION. Please run 'source ~/.bashrc' to start using Procs."
